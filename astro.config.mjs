931c7e1d920d37ab0e13ca311a4fa80e828e7c7c
Thiago2104 <santijipin@gmail.com>
2025-05-04 14:45:56 -0500

    Add 3d model

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -24,6 +22,4 @@ export default defineConfig({
       })
     }
   },
-
-  adapter: vercel(),
 });
